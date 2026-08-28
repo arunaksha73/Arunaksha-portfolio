6266166383cc852fe8eda0e0c15b5849a80f4b17
Arunaksha73 <arunaksha73@gmail.com>
2026-08-28 21:12:10 +0530

    Add Google Search Console verification

--- fix_paths.py
+++ fix_paths.py
@@ -5,6 +5,7 @@ with open('portfolioindex.html', 'r', encoding='utf-8') as f:
 
 # Replace image paths: src="./sunset.jpeg" -> src="Portfolio/sunset.jpeg"
 html = re.sub(r'src="\./(sunset|durgamaa|flower|pot_PM|bonds)\.jpeg"', r'src="Portfolio/\1.jpeg"', html)
+html = html.replace('src="./20260630_134632.jpg"', 'src="Portfolio/20260630_134632.jpg"')
 
 # Replace other relative links
 html = html.replace('href="blog.html"', 'href="Portfolio/blog.html"')
